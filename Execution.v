eabe65320cac254e6c11241c49a9af9a41558a49
Karthik <162018758+KarthikPerepu@users.noreply.github.com>
2025-05-11 22:00:05 +0530

    Update Execution.v

--- Execution.v
+++ Execution.v
@@ -436,3 +436,23 @@ assign fa=isBeq&flag[0];
 assign sa=isBgt&flag[1];
 assign isBranchTaken=isUbranch|fa|sa|isCall;
 endmodule
+//Flag_Register
+module flag_reg (
+input clk, 
+  input isCmp,
+  input cmp_g,
+  input cmp_e,
+  output reg ocmp_g,
+  output reg ocmp_e 
+
+);
+always @(posedge clk)
+begin
+if(isCmp)
+begin
+  ocmp_g<=cmp_g;
+  ocmp_e<=cmp_e;
+end
+end
+
+endmodule
